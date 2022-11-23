cfd812df6e7fabf02890dba43094ec80b2959169
samcms1234 <samdevpro99@gmail.com>
2022-11-23 23:20:49 +0530

    hardhat config updated

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,8 +1,34 @@
-require("@nomicfoundation/hardhat-toolbox");
-require("@nomicfoundation/hardhat-chai-matchers");
+require("@nomiclabs/hardhat-waffle");
 require("@nomiclabs/hardhat-ethers");
+const fs = require('fs');
+require('dotenv').config();
 
-/** @type import('hardhat/config').HardhatUserConfig */
+task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
+  const accounts = await hre.ethers.getSigners();
+
+  for (const account of accounts) {
+    console.log(account.address);
+  }
+});
+
 module.exports = {
-  solidity: "0.8.17",
+  defaultNetwork: "hardhat",
+  networks: {
+    hardhat: {
+      chainId: 1337
+    },
+    goerli: {
+      url: process.env.URL.toString(),
+      accounts: [ process.env.PRIVATE_KEY.toString() ]
+    }
+  },
+  solidity: {
+    version: "0.8.4",
+    settings: {
+      optimizer: {
+        enabled: true,
+        runs: 200
+      }
+    }
+  }
 };
